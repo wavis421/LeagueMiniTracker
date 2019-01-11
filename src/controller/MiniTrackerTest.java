a6dfd3e39f1d52b007bf9754085796eb286f98e2
wavis421 <wendy.avis@jointheleague.org>
2019-01-11 11:37:08 -0700

    Add comments.

--- src/controller/MiniTrackerTest.java
+++ src/controller/MiniTrackerTest.java
@@ -5,6 +5,15 @@ import java.util.ArrayList;
 import model.MiniTrackerDatabase;
 import model.StudentMiniModel;
 
+/**
+ * Mini Tracker Test:
+ * 
+ *      This is sample code for testing the Student Mini Trackr Database.
+ *      Final code would export the 'model' package to access the data.
+ * 
+ * @author wavis
+ *
+ */
 public class MiniTrackerTest {
 	public static void main(String[] args) {
 		// Test all API's for Mini Tracker database
